07a2b1a84ddb9e3cf5f7f2c86871cf076f245ac6
Serquand <esteban.vincent@efrei.net>
2024-04-04 08:45:23 +0200

    Feat: Add Activity

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,9 +1,15 @@
+const { Client } = require('discord.js');
 const discordConfig = require('../config/discord.config.js');
 
 module.exports = {
     name: "ready",
     once: true,
+    /**
+     *
+     * @param {Client} client
+     */
     async execute (client) {
+        client.user.setActivity('Développé par @serquand')
         const allGuilds = discordConfig.guilds;
         for(const guildId of allGuilds) {
             const guild = client.guilds.cache.get(guildId);
